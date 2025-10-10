74770fc2a947f71768bd7c0e408fd570ada432c2
John Byrd <johnwbyrd@gmail.com>
2025-10-10 16:50:03 -0700

    Clean up pytest

--- src/state/__init__.py
+++ src/state/__init__.py
@@ -3,10 +3,8 @@
 from src.state.attempt import MergeAttempt
 from src.state.build import BuildResult
 from src.state.conflict import ConflictInfo, ConflictResolution
-from src.state.workflow import MergeWorkflowState
 
 __all__ = [
-    "MergeWorkflowState",
     "ConflictInfo",
     "ConflictResolution",
     "MergeAttempt",
